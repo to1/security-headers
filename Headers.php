3e31a7100ac20ca891798020b823b72b367d44e6
Jeton Ahmetaj <jeton.ahmetaj40@gmail.com>
2019-07-01 16:37:13 +0200

    Update Headers.php

--- Headers.php
+++ Headers.php
@@ -14,6 +14,8 @@ class Header
     {
         $this->removeUnwantedHeaders($this->removeHeaderList);
         $response = $next($request);
+        
+        //A list of the headers you want to include and they'r options
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
